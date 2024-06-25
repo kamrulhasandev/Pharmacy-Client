7db3b84fc52ad8bf4f29f90d2315249026b47f64
kamrul <kamrul444216@gmail.com>
2024-06-25 23:21:26 +0600

    dashboard sidebar ui design

---
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardHome = () => {
+  return (
+    <div>
+      <h1>This is DashboardHome component</h1>
+    </div>
+  )
+};
+
+export default DashboardHome;
---
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+import "./layout.css";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="dashboard-layout">
+      <header className="header">
+        <div className="header-content">
+          <button className="toggle-button" onClick={toggleSidebar}>
+            ☰
+          </button>
+        </div>
+      </header>
+
+      <aside className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="logo">
+          <a href="/">MedLane</a>
+        </div>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <a href="/">
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a href="/add-category">
+                <p>Add Category</p>
+              </a>
+            </li>
+            <li>
+              <a href="/all-category">
+                <p>All Category</p>
+              </a>
+            </li>
+            <li>
+              <a href="/add-product">
+                <p>Add Product</p>
+              </a>
+            </li>
+            <li>
+              <a href="/all-product">
+                <p>All Product</p>
+              </a>
+            </li>
+            <li>
+              <a href="/all-product">
+                <p>Users</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+
+      <main className="main-content">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
